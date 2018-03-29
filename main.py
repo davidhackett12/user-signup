29aa643df00337f22bb9f11ffb34010e93fa40f1
David Hackett <DHackett@STC.LOCAL>
2018-03-29 13:18:46 -0500

    created a redirect for username

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ app.config['DEBUG'] = True
 def index():
     return render_template('index.html')
 
-@app.route('/success', methods=['POST'])
+@app.route("/success", methods=['POST'])
 def success():
     username = request.form['username']
+    if len(username) <= 3:
+        return redirect("/")
+
     return render_template('success.html', username=username)
 
 app.run()
