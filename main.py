2ec6942921110e1fea2e4d2e2cc93b0327148ed9
David Hackett <DHackett@STC.LOCAL>
2018-03-29 19:29:56 -0500

    will validate for username and password, but error messages are still off

--- main.py
+++ main.py
@@ -5,16 +5,41 @@ import os
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    username_error = ""
+    username_error = request.args.get(username_error)
+    if username_error:
+        username_error = username_error
+    else:
+        username_error = ""
+    return render_template('index.html', username_error=username_error)
 
 @app.route("/success", methods=['POST'])
 def success():
     username = request.form['username']
-    if len(username) <= 3:
-        return redirect("/")
+    password = request.form['password']
+    if len(username) <= 3 or len(username) >= 20:
+        username_error = "username is not the right length"
+    elif " " in username:
+        username_error = "username cannot include spaces"
+    elif username == "":
+        usernname_error = "no username is entered" 
+    else:
+        username_error = ""
 
-    return render_template('success.html', username=username)
+    if password == "":
+        password_error = "you did not enter a password"
+    elif len(password) <= 3 or len(password) >= 20:
+        password_error = "password is not the right length"
+    else:
+        password_error = ""
+
+
+    if username_error or password_error:
+        return redirect("/?=", username_error=username_error, password_error= password_error)
+    else:
+        return render_template('success.html', username=username)
 
 app.run()
