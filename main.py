d066bd3f9852f890b11037d89576cd65f1485e2b
David Hackett <DHackett@STC.LOCAL>
2018-04-02 08:54:17 -0500

    started to change main program to render rather than redirect

--- main.py
+++ main.py
@@ -9,18 +9,26 @@ app.config['DEBUG'] = True
 @app.route("/")
 def index():
     username_error = ""
+    password_error = ""
+    retype_error = ""
+    email_error = ""
     username_error = request.args.get(username_error)
-    if username_error:
+    password_error = request.args.get(password_error)
+    if username_error or password_error:
         username_error = username_error
+        password_error = password_error
     else:
         username_error = ""
-    return render_template('index.html', username_error=username_error)
+        password_error = ""
+    return render_template('index.html', username_error=username_error, password_error = password_error)
 
 @app.route("/success", methods=['POST'])
 def success():
     username = request.form['username']
     password = request.form['password']
-    if len(username) <= 3 or len(username) >= 20:
+    retype_password = request.form['retype_password']
+    email = request.form['email']
+    if len(username) <= 2 or len(username) >= 21:
         username_error = "username is not the right length"
     elif " " in username:
         username_error = "username cannot include spaces"
@@ -31,14 +39,24 @@ def success():
 
     if password == "":
         password_error = "you did not enter a password"
-    elif len(password) <= 3 or len(password) >= 20:
+    elif len(password) <= 2 or len(password) >= 21:
         password_error = "password is not the right length"
     else:
         password_error = ""
+    
+    if retype_password != password:
+        retype_error = "passwords do not match"
+    else:
+        retype_err0r = ""
+    
+    if "@" or "." not in email:
+        email_error = "this is not a valid email"
+    else:
+        email_error = ""
 
 
-    if username_error or password_error:
-        return redirect("/?=", username_error=username_error, password_error= password_error)
+    if username_error or password_error or retype_error or email_error:
+        return render("/", username_error=username_error, password_error=password_error)
     else:
         return render_template('success.html', username=username)
 
